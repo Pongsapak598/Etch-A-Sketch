46ae679a094ecb15840acd71ca4565032d87866a
Pongsapak598 <pongsapak.roc@gmail.com>
2023-03-20 20:32:21 +0700

    Fixed some bug with outline gets thinner after colored

--- script.js
+++ script.js
@@ -19,9 +19,15 @@ function generateGrid() {
 
         gridDiv.style.width = `${divSize}px`;
         gridDiv.style.height = `${divSize}px`;
-        gridDiv.style.outline = "1px solid black";
+        gridDiv.style.outline = "2px solid black"
         gridDiv.style.cssFloat = "left";
-
+        gridDiv.style.boxSizing = "border-box";
+        gridDiv.style.background = 'white'
+        
+        gridDiv.onmouseover = function() {
+            this.style.background = 'rgb(175, 255, 187)';
+          };
+          
         container.appendChild(gridDiv)
     }
 }
