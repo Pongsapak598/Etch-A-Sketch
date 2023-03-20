0a8461aa0183db4dd0f5c7d82b294138adc02334
Pongsapak598 <pongsapak.roc@gmail.com>
2023-03-20 22:38:28 +0700

    Full version with GUI with random color and black dot every 10 colors

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function calDivSize() {
 
 const container = document.querySelector("#container")
 
-function generateGrid() {
 
+function generateGrid() {
     for (let i = 0 ; i < userNumber * userNumber ; i++) {
         const gridDiv = document.createElement("div");
 
@@ -23,17 +23,42 @@ function generateGrid() {
         gridDiv.style.cssFloat = "left";
         gridDiv.style.boxSizing = "border-box";
         gridDiv.style.background = 'white'
-        
-        gridDiv.onmouseover = function() {
-            this.style.background = 'rgb(175, 255, 187)';
-          };
-          
+        gridDiv.classList.add("grid-div");
+
         container.appendChild(gridDiv)
     }
+}
+
+let colorCount = '0';
+
+function colorGrid() {
+    function randomColor() {
+        const random = Math.floor(Math.random()*16777215).toString(16);
+        if (random == 'FFFFFF' || random == '000000') {
+            random = '123456';
+            return random
+        } else return random
+    }
+
+    const grids = document.querySelectorAll(".grid-div");
+    grids.forEach(grid => {
+        grid.addEventListener('mouseover', function() {
+            if (colorCount%10 === 0) {
+                if (this.style.background == 'white')
+                    this.style.background = 'black';
+                ++colorCount;
+            } else if (this.style.background == 'white') {
+                this.style.background = `#${randomColor()}`;
+                ++colorCount;
+            }
+        })
+    })
 }
 
 function removeAllChilds(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+
+    colorCount = '0';
 }
